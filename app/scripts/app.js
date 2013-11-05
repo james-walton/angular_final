6503b3be2d21f4bc33967b32d8a97b680ad19e42
TheKnightArgent <github@jsiii.com>
2013-11-04 19:33:12 -0800

    Initial
    Bare scaffolding from yo

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('angularFinalApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
